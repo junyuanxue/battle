8fc45dbab912ac716b38291e4406f5cc4db9bdb4
Junyuan Xue <junyuanxue.is@gmail.com>
2016-03-15 19:45:58 +0000

    Tidying up 25

--- spec/features/names_spec.rb
+++ spec/features/names_spec.rb
@@ -3,6 +3,6 @@ require './app'
 feature "Enter names" do
   scenario "User can enter a name" do
     sign_in_and_play
-    expect(page).to have_text("Kevin vs Fluffy.")
+    expect(page).to have_content("Kevin vs Fluffy.")
   end
 end
