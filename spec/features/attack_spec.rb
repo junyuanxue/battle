a21c6cb282c5f729d791ba65c893486f97900482
Junyuan Xue <junyuanxue.is@gmail.com>
2016-03-18 17:30:52 +0000

    Refactored

--- spec/features/attack_spec.rb
+++ spec/features/attack_spec.rb
@@ -1,16 +1,3 @@
-<<<<<<< HEAD
-feature 'Attack opponent' do
-
-  before do
-    sign_in_and_play
-    visit '/play'
-    allow(Kernel).to receive(:rand).and_return 2
-    click_link('Attack!')
-  end
-
-  scenario 'P1 attacks P2 and P2 loses HP' do
-    expect(page).to have_content "Pete HP: 18"
-=======
 feature 'Attack' do
   before do
     sign_in_and_play
@@ -20,18 +7,12 @@ feature 'Attack' do
   scenario 'P1 selects basic attack and P2 loses HP' do
     click_button "Basic Attack"
     expect(page).to have_content "Pete HP: 8"
->>>>>>> day-four
   end
 
   scenario 'P2 attacks P1 and P1 loses HP' do
     click_button "Basic Attack"
     click_link('Next round!')
-<<<<<<< HEAD
-    click_link('Attack!')
-    expect(page).to have_content "Misa HP: 18"
-=======
     click_button('Basic Attack')
     expect(page).to have_content "Misa HP: 8"
->>>>>>> day-four
   end
 end
